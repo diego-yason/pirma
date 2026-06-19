8a14786963ea1f6bdd5075b7e7e3a570c63c7ad6
diego-yason <61825575+diego-yason@users.noreply.github.com>
2026-06-19 21:41:28 +0800

    feat: enhance navigation bar styling and improve text hierarchy

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -4,17 +4,17 @@
     let { children } = $props();
 </script>
 
-<nav class="flex justify-between px-8 py-3">
+<nav class="flex justify-between px-8 py-3 border-b mb-4">
     <div class="flex gap-4 items-center">
-        <h1 class="font-bold text-xl">Prima</h1>
-        <a class="font-semibold tracking-wide" href={resolve("/features")}>Features</a>
-        <a class="font-semibold tracking-wide" href={resolve("/blockchain")}>Blockchain</a>
-        <a class="font-semibold tracking-wide" href={resolve("/security")}>Security</a>
-        <a class="font-semibold tracking-wide" href={resolve("/pricing")}>Pricing</a>
+        <a class="font-bold text-xl" href={resolve("/")}>Prima</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/features")}>Features</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/blockchain")}>Blockchain</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/security")}>Security</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/pricing")}>Pricing</a>
     </div>
     <div class="flex gap-4 items-center">
-        <a class="font-semibold tracking-wide" href={resolve("/login")}>Log In</a>
-        <a class="font-semibold tracking-wide" href={resolve("/register")}>Get Started</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/login")}>Log In</a>
+        <a class="font-medium text-sm tracking-wide" href={resolve("/register")}>Get Started</a>
     </div>
 </nav>
 
